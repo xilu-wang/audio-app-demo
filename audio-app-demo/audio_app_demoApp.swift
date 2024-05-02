fe36b447a0cf511fb1e1fcdf03d16f7924205d1d
Xilu Wang <kate99w@gmail.com>
2024-05-01 21:46:12 -0700

    Initial Commit

---
+++ audio-app-demo/audio_app_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_app_demoApp.swift
+//  audio-app-demo
+//
+//  Created by Kate W. on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct audio_app_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ audio-app-demoUITests/audio_app_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  audio_app_demoUITestsLaunchTests.swift
+//  audio-app-demoUITests
+//
+//  Created by Kate W. on 5/1/24.
+//
+
+import XCTest
+
+final class audio_app_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
